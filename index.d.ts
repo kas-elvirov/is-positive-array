c807793aa257836e8d787dc07f178815773f54b0
Kas Elvirov <kas.elvirov@gmail.com>
2022-10-01 04:02:11 +0300

    Rebuild

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,2 @@
 declare function isPositiveArray(incomingValue: unknown): boolean;
-export { isPositiveArray, };
 export default isPositiveArray;
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPositiveArray = void 0;
 function isPositiveArray(incomingValue) {
     return Array.isArray(incomingValue)
         && incomingValue.length > 0;
 }
-exports.isPositiveArray = isPositiveArray;
 exports.default = isPositiveArray;
